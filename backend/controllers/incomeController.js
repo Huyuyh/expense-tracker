e272fc599ffe0d2a22fb044b45cd9779dc228cec
huyuyh <phamhuy09042002@gmail.com>
2026-01-28 16:59:49 -0500

    add income route

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,11 @@
+// Add Income
+exports.addIncome = async (req, res) => {};
+
+// Get All Income
+exports.getAllIncome = async (req, res) => {};
+
+// Delete Income
+exports.deleteIncome = async (req, res) => {};
+
+// Download Income Excel
+exports.downloadIncomeExcel = async (req, res) => {};
