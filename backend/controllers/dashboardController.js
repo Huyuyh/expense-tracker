71225b4c57e3f09cdf33479848f05268b13fa9ff
huyuyh <phamhuy09042002@gmail.com>
2026-01-29 18:00:04 -0500

    get data dashboard

---
+++ backend/controllers/dashboardController.js
@@ -0,0 +1,63 @@
+const Expense = require("../models/Expense");
+const Income = require("../models/Income");
+const { isValidObjectId, Types } = require("mongoose");
+
+// Get Dashboard Data
+exports.getDashboardData = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const userId = req.user.id;
+
+    const userObjectId = new Types.ObjectId(String(userId));
+
+    //Fetch total income & expenses
+    const totalIncome = await Income.aggregate([
+      { $match: { userId: userObjectId } },
+      { $group: { _id: null, total: { $sum: "$amount" } } },
+    ]);
+
+    console.log("totalIncome", {
+      totalIncome,
+      userId: isValidObjectId(userId),
+    });
+
+    const totalExpense = await Expense.aggregate([
+      { $match: { userId: userObjectId } },
+      {
+        $group: {
+          _id: null,
+          total: {
+            $sum: "$amount",
+          },
+        },
+      },
+    ]);
+
+    // Get income transactions in the last 60 days
+    const last60DaysIncomeTransactions = await Income.find({
+      userId,
+      date: {
+        $gte: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000),
+      },
+    }).sort({
+      date: -1,
+    });
+
+    // Get total income for last 60 days
+    const incomeLast60Days = last60DaysIncomeTransactions.reduce(
+      (sum, transaction) => sum + transaction,
+      0,
+    );
+
+    // Get expense transactions in the last 30 days
+    const last30DaysExpenseTransactions = await Expense.find({
+      userId,
+      date: {
+        $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
