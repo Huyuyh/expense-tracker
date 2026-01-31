c0f3f6686e515fa23492384ed3175f4aaf513736
huyuyh <phamhuy09042002@gmail.com>
2026-01-30 20:16:48 -0500

    comment

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -2,6 +2,7 @@ const Expense = require("../models/Expense");
 const Income = require("../models/Income");
 const { isValidObjectId, Types } = require("mongoose");
 
+// Get Dashboard Data
 exports.getDashboardData = async (req, res) => {
   const userId = req.user.id;
 
